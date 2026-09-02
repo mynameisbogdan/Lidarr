aee2bd6e46da4c4cc81ae57a9503cb03d639e445
Bogdan <mynameisbogdan@users.noreply.github.com>
2026-09-02 12:52:11 +0300

    Revert "Improved PluginService with tree support"
    This reverts commit f6a3e7370540cc25caf3aaf0f1c91e7c085585ac.

--- src/NzbDrone.Core/Plugins/Plugin.cs
+++ src/NzbDrone.Core/Plugins/Plugin.cs
@@ -1,5 +1,4 @@
-using System.Reflection;
-using NzbDrone.Common.Extensions;
+using System;
 
 namespace NzbDrone.Core.Plugins
 {
@@ -8,41 +7,18 @@ namespace NzbDrone.Core.Plugins
         string Name { get; }
         string Owner { get; }
         string GithubUrl { get; }
-        PluginVersion InstalledVersion { get; }
-        PluginVersion AvailableVersion { get; set; }
+        Version InstalledVersion { get; }
+        Version AvailableVersion { get; set; }
     }
 
     public abstract class Plugin : IPlugin
     {
-        private PluginVersion _installedVersion;
-
         public virtual string Name { get; }
         public virtual string Owner { get; }
         public virtual string GithubUrl { get; }
 
-        public PluginVersion InstalledVersion
-        {
-            get
-            {
-                if (_installedVersion != null)
-                {
-                    return _installedVersion;
-                }
-
-                var informationalVersion = GetType().Assembly
-                    .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
-                    .InformationalVersion;
-
-                if (informationalVersion.IsNotNullOrWhiteSpace())
-                {
-                    _installedVersion = PluginVersion.Parse(informationalVersion);
-                }
-
-                return _installedVersion ??= GetType().Assembly.GetName().Version;
-            }
-        }
-
-        public PluginVersion AvailableVersion { get; set; }
+        public Version InstalledVersion => GetType().Assembly.GetName().Version;
+        public Version AvailableVersion { get; set; }
     }
 
     public class RemotePlugin
@@ -50,8 +26,7 @@ namespace NzbDrone.Core.Plugins
         public string Name { get; set; }
         public string Owner { get; set; }
         public string GithubUrl { get; set; }
-        public PluginVersion Version { get; set; }
+        public Version Version { get; set; }
         public string PackageUrl { get; set; }
-        public string Tree { get; set; }
     }
 }
